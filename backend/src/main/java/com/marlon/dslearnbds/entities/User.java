af99c11a4156fc858e9c370ff39bffe820f0bf7c
marlon-frazao <frazao.marlon@gmail.com>
2021-04-28 14:17:25 -0300

    Topic

--- backend/src/main/java/com/marlon/dslearnbds/entities/User.java
+++ backend/src/main/java/com/marlon/dslearnbds/entities/User.java
@@ -37,6 +37,9 @@ public class User implements Serializable {
 	@OneToMany(mappedBy = "user")
 	private List<Notification> notifications = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "author")
+	private List<Topic> topics = new ArrayList<>();
+	
 	public User() {}
 
 	public User(Long id, String name, String email, String password, Set<Role> roles) {
@@ -85,6 +88,10 @@ public class User implements Serializable {
 
 	public List<Notification> getNotifications() {
 		return notifications;
+	}
+
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
